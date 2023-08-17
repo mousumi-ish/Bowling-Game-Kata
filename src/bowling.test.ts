7a1981a5c301745898ab017a7e0091b70b49087f
mousumi-ish <baral_mousumi@yahoo.com>
2023-08-17 15:04:26 +0100

    feat : passed test for strike

--- src/bowling.test.ts
+++ src/bowling.test.ts
@@ -1,4 +1,5 @@
 import { bowlingScore } from "./bowling";
+import { Frame } from "./bowling";
 describe("Gutter game", () => {
   test("it will return an error msg for all 0 values", () => {
     const inputRolls = [
@@ -7,4 +8,10 @@ describe("Gutter game", () => {
     const score = bowlingScore(inputRolls); //ACT
     expect(score).toBe(0); //ASSERT
   });
+});
+test("It should return strike if the value of first roll is 10", () => {
+  const rollOne = 10;
+  const rollTwo = 0;
+  const score = Frame(rollOne, rollTwo);
+  expect(score).toBe("X");
 });
--- src/bowling.ts
+++ src/bowling.ts
@@ -1,3 +1,10 @@
 export const bowlingScore = (inputRolls: Number[]) => {
   return 0;
 };
+export const Frame = (rollOne: number, rollTwo: number) => {
+  const score = rollOne + rollTwo;
+  if (score > 10) {
+    return " Invalid input";
+  }
+  if (rollOne === 10) return "X";
+};
